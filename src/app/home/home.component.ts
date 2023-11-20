99f72c3461a91c9c7587a963ffbb13e9e6031d01
Nathanael Santana <nathanaelotaku28@gmail.com>
2023-11-20 19:21:06 -0300

    Implemeted the search house for the city

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,14 +15,26 @@ import { HousingService } from '../housing.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {  
-  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
-
   housingLocationList: Housinglocation[] = [];
-
   housingService: HousingService = inject(HousingService);
+  filteredLocationList: Housinglocation[] = [];
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string){
+    console.log('lista reserva: ', this.filteredLocationList);
+    if(!text){
+      this.filteredLocationList = this.housingLocationList;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) => 
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+    );
+
+    console.log('lista reserva preenchida: ', this.filteredLocationList);
   }
 
 }
